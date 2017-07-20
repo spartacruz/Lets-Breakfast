9576359bd975b7adbb3175baa0c03722323a2352
Yuri <yuri.barru@gmail.com>
2017-07-20 20:52:53 +0700

    Creating an alert to user for double check & fill in all the delivery detail before pressing 'buy now' button.

--- app/src/main/java/com/example/android/letsbreakfast/FillInDetailActivity.java
+++ app/src/main/java/com/example/android/letsbreakfast/FillInDetailActivity.java
@@ -1,8 +1,15 @@
 package com.example.android.letsbreakfast;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.example.android.letsbreakfast.OrderFood1Activity.quantity;
 
@@ -127,6 +134,65 @@ public class FillInDetailActivity extends AppCompatActivity {
 
         // Set text on FOOD GRAND TOTAL view after calculating the price
         foodGrandTotal.setText(String.valueOf(price));
+    }
+
+    /**
+     * This method is called when user press next button / buy now
+     * <p>
+     * will show a warning before proceed
+     */
+    public void warning(View view) {
+        // Find the View that shows the NAME TEXT INPUT
+        EditText nameTextInput = (EditText) findViewById(R.id.fid_name_text_input);
+        // Find the View that shows the PHONE TEXT INPUT
+        EditText phoneTextInput = (EditText) findViewById(R.id.fid_phone_text_input);
+        // Find the View that shows the ADDRESS TEXT INPUT
+        EditText addressTextInput = (EditText) findViewById(R.id.fid_address_text_input);
+
+        //if statement to make sure that users are entering their name, phone, and address correctly based on character length
+        //if NAME TEXT INPUT field only have less than 1 character, then show a toast.
+        if (nameTextInput.getText().toString().length() < 1) {
+            Toast.makeText(this, "Please enter a valid name",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            //else, if PHONE TEXT INPUT field only have less or equal than 5 character, then show a toast.
+            if (phoneTextInput.getText().toString().length() <= 5) {
+                Toast.makeText(this, "Please enter a valid phone number",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                //else, if ADDRESS TEXT INPUT field only have less or equal than 5 character, then show a toast.
+                if (addressTextInput.getText().toString().length() <= 5) {
+                    Toast.makeText(this, "Please enter a valid address",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    //else, show an alert dialog.
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Warning");
+                    builder.setMessage("Please make sure that all shipping details is correct.\nDo you wish to proceed?");
+
+                    //when users press "yes", they will move to new intent (activity_purchasehasbeenmade)
+                    builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent intent = new Intent(FillInDetailActivity.this, PurchaseHasBeenMadeActivity.class);
+                            startActivity(intent);
+
+                        }
+                    });
+
+                    AlertDialog dialog = builder.create();
+
+                    //when users press "No", alert dialog will dismiss.
+                    builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.show();
+                }
+            }
+        }
+
     }
 
     /**
