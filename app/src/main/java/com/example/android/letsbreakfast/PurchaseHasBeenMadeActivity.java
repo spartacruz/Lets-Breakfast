0b6dbfdadc7de004018f0f75d3d3e08c0000fb10
Yuri <yuri.barru@gmail.com>
2017-07-20 20:49:27 +0700

    Adding new intent, for indicate users that purchase has been made.

---
+++ app/src/main/java/com/example/android/letsbreakfast/PurchaseHasBeenMadeActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.letsbreakfast;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by phi3d on 7/20/2017.
+ */
+
+public class PurchaseHasBeenMadeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchasehasbeenmade);
+    }
+}
